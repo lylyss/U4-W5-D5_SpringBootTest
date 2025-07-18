6ad5f48382db015ff7df5c17543706611d085c20
Loboda Nicolae <77647358+lylyss@users.noreply.github.com>
2025-07-18 15:50:19 +0200

    Aggiunta metodi per la gestione degli edifici in EdificioRepository

--- src/main/java/LobodaNicolae/U4_W5_D5_SpringBootTest/Repository/EdificioRepository.java
+++ src/main/java/LobodaNicolae/U4_W5_D5_SpringBootTest/Repository/EdificioRepository.java
@@ -3,5 +3,10 @@ package LobodaNicolae.U4_W5_D5_SpringBootTest.Repository;
 import LobodaNicolae.U4_W5_D5_SpringBootTest.Entities.Edificio;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EdificioRepository extends JpaRepository<Edificio, Long> {
+    boolean existsByNome(String nome);
+
+    List<Edificio> findByNomeContainingIgnoreCase(String nome);
 }
